f7e54e29cfeb44316fe04e105d33314f53b85be3
malejandros <isc.malejandro@gmail.com>
2019-03-28 11:39:01 -0600

    lista de iconos utilizando listview

--- flutter_category/lib/main.dart
+++ flutter_category/lib/main.dart
@@ -12,37 +12,61 @@ class MyApp extends StatelessWidget {
 }
 
 Widget myCategory(){
-  return Container(
-    height: 100.0,
-    color: const Color.fromRGBO(255, 255, 150, 1.0),
-    padding: EdgeInsets.all(2.0),
-    child: Material(
-      child: InkWell(
-        borderRadius: BorderRadius.circular(50.0),
-        child: Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Icon(
-                IconData(58126, fontFamily: 'MaterialIcons'),
-                size: 60.0,
-              ),
-            ),
-            Center(
-              child: Text(
-                'My icon',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 24),
-              ),
-            )
-          ],
+  return Scaffold(
+    appBar: AppBar(
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(
+          fontSize: 30.0,
         ),
-        onTap: () {
-          print('I was tapped');
-        },
       ),
     ),
-    );
+    body: Container(
+      height: 400.0,
+      color: const Color.fromRGBO(255, 255, 150, 1.0),
+      padding: EdgeInsets.all(2.0),
+      child: Material(
+        child: InkWell(
+          borderRadius: BorderRadius.circular(50.0),
+          child: ListView(
+            children: <Widget>[
+              makeIcons(58126),
+              makeIcons(59670),
+              makeIcons(58311),
+              makeIcons(59506),
+              makeIcons(59691),
+              makeIcons(59694),
+              makeIcons(57708),
+            ],
+          ),
+          onTap: () {
+            print('I was tapped');
+          },
+        ),
+      ),
+    ),
+  );
+}
+
+Widget makeIcons(num noIcon){
+  return Row(
+    children: <Widget>[
+      Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Icon(
+          IconData(noIcon, fontFamily: 'MaterialIcons'),
+          size: 60.0,
+        ),
+      ),
+      Center(
+        child: Text(
+          'My icon',
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24),
+        ),
+      )
+    ],
+  );
 }
 
 
