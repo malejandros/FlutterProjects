4c13147410c65f273974d9229f79f7542b0c31e7
malejandros <isc.malejandro@gmail.com>
2019-03-27 23:41:32 -0600

    ejemplo tomado de codelabs

---
+++ app1_codelabs/android/app/src/main/java/chacomoc/app1_codelabs/MainActivity.java
@@ -0,0 +1,13 @@
+package chacomoc.app1_codelabs;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
