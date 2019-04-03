fe83c38c62fbecb24ee024ef0b4e58018c39e779
malejandros <isc.malejandro@gmail.com>
2019-04-03 11:32:39 -0600

    ejemplo usando un drawer

---
+++ app_drawer/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My page'),
+        ),
+        body: Center(
+          child: Text('My page'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {},
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
